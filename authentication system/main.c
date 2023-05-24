0c924ecd2c8b8cc62d7472bb3ac40f7b13a6a8e5
Himanshu Patidar <112263082+himanshupatidar28@users.noreply.github.com>
2023-05-24 14:48:07 +0530

    Add files via upload

---
+++ authentication system/main.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+# include<stdlib.h>
+# include"login.c"
+# include"nav.c"
+
+int main(){
+    
+    printf("\t\t WELCOME TO AUTHENTICATION SYSTEM \t\t\n");
+    printf("\t\t -------------------------------- \t\t\n");
+    nav();
+    
+    return 0;
+}
